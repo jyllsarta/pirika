31f62177a0dcdb86f40307e2eca44179428cc537
jyllsarta <illstarred.dive@gmail.com>
2018-08-08 22:20:49 +0900

    RemoのAPIをコールできる

--- app/controllers/pirika_console_controller.rb
+++ app/controllers/pirika_console_controller.rb
@@ -1,7 +1,25 @@
+require 'yaml'
+
 class PirikaConsoleController < ApplicationController
   def index
   end
   def update
-    puts :pi
+    puts "Pi!" 
+    yaml = YAML.load_file("config/pirika_secrets.yml")
+    call_api yaml["token"], yaml["signals"]["fan_on"]
+  end
+
+private
+  def call_api(token, signal_id)
+
+    url = "https://api.nature.global/1/signals/#{signal_id}/send"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = "application/json"
+    req["Authorization"] = "Bearer #{token}"
+    req.body = ""
+    res = http.request(req)
   end
 end
