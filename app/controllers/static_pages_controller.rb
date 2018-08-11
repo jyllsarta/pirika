19d95f95b16eb17bbbe612c1c39505303f1439d9
jyllsarta <illstarred.dive@gmail.com>
2018-08-12 00:03:57 +0900

    jyllsarta.github.ioの静的ページ移植先の仮置き

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,43 @@
 class StaticPagesController < ApplicationController
   def show
   end
+
+  def articles
+  end
+
+  def contacts
+  end
+
+  def illusts
+  end
+
+  def index
+  end
+
+  def neko
+  end
+
+  def neko2
+  end
+
+  def nekomanual
+  end
+
+  def sainokawara
+  end
+
+  def samusugi
+  end
+
+  def top
+  end
+
+  def touzoku
+  end
+
+  def twitter_webhook
+  end
+
+  def works
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,17 @@ Rails.application.routes.draw do
 
   get 'static_pages/show'
   root 'static_pages#show'
+
+  get 'neko/',  to: "static_pages#neko"
+  get 'neko2/',  to: "static_pages#neko2"
+  get 'nekomanual/',  to: "static_pages#nekomanual"
+  get 'sainokawara/',  to: "static_pages#sainokawara"
+  get 'samusugi/',  to: "static_pages#samusugi"
+  get 'top/',  to: "static_pages#top"
+  get 'touzoku/',  to: "static_pages#touzoku"
+  get 'twitter_webhook/',  to: "static_pages#twitter_webhook"
+  get 'works/',  to: "static_pages#works"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
