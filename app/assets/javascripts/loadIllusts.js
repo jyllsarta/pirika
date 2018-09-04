350ec4e1e6a0dbe3988bd66c87a8824b6dfb107b
jyllsarta <illstarred.dive@gmail.com>
2018-09-05 00:08:42 +0900

    最近のイラスト追加

--- app/assets/javascripts/loadIllusts.js
+++ app/assets/javascripts/loadIllusts.js
@@ -1,8 +1,8 @@
-function loadIllusts(){
-	var count = 125
-	for(var i=count; i>0; --i){
+function loadIllusts() {
+	var count = 135
+	for (var i = count; i > 0; --i) {
 		$("#illustBox").append(`<a href="images/illust_all/${i}.png"><div class="thumb"></div>`)
-		$(".thumb:last").css("background-image",`url("images/illust_thumbnails/${i}.png")`)
+		$(".thumb:last").css("background-image", `url("images/illust_thumbnails/${i}.png")`)
 	}
 }
 
