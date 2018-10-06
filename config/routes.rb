6e6856babd98575ca2bcc4441a0e24ebdf37ff17
jyllsarta <illstarred.dive@gmail.com>
2018-10-07 01:12:01 +0900

    メールフォームをrails側で処理するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get 'index/',       to: "static_pages#index"
   get 'samusugi/',    to: "static_pages#samusugi"
   get 'works/',       to: "static_pages#works"
-  get 'contact/',     to: "static_pages#contact"
   get 'illusts/',     to: "static_pages#illusts"
   get 'sainokawara/', to: "static_pages#sainokawara"
   get 'neko/',        to: "static_pages#neko"
@@ -15,7 +14,9 @@ Rails.application.routes.draw do
   get 'neko2/',       to: "static_pages#neko2"
   get 'snipon/',       to: "static_pages#snipon"
   get 'touzoku/',     to: "static_pages#touzoku"
-  get 'twitter_webhook/',  to: "static_pages#twitter_webhook"
+  get 'twitter_webhook/', to: "static_pages#twitter_webhook"
+  get 'contact/',     to: "contacts#new"
+  post 'contact/',    to: "contacts#create"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
