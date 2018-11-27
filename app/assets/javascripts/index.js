b7c3f36bd13b7a72d1f28419170fc2e32e6b5598
jyllsarta <illstarred.dive@gmail.com>
2018-11-27 22:56:28 +0900

    タブ文字撲滅委員会

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
-	$("#top_illust").animate({"opacity":1},4000,"linear");
-	$("#top_link").animate({'bottom':'10%','opacity':'1'},1000,'easeOutExpo');	    	
+$(document).ready(function () {
+    $("#top_illust").animate({ "opacity": 1 }, 4000, "linear");
+    $("#top_link").animate({ 'bottom': '10%', 'opacity': '1' }, 1000, 'easeOutExpo');
 });
