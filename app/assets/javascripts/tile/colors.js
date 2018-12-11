1b7aced3402b6031b912ce37ffb091786e2af586
jyllsarta <illstarred.dive@gmail.com>
2018-12-11 23:27:04 +0900

    青を微調整

--- app/assets/javascripts/tile/colors.js
+++ app/assets/javascripts/tile/colors.js
@@ -1,5 +1,5 @@
 let g_colors = {
-    0: "linear-gradient(to bottom, #bee3ff 0%,#bee3ff 53%,#afd8ff 57%,#838fff 100%)",
+    0: "linear-gradient(to bottom, #9dd4ff 0%,#a7d6f8 53%,#7aafe0 57%,#6774ec 100%)",
     1: "linear-gradient(to bottom, #fabbff 0%,#fabbff 53%,#ff9fff 57%,#ff84cc 100%)",
     2: "linear-gradient(to bottom, #ffbdae 0%,#ffbdae 53%,#ffb29a 57%,#ffdd9d 100%)",
     3: "linear-gradient(to bottom, #ffe693 0%,#ffec9f 53%,#ffe57e 57%,#f0ffb8 100%)",
