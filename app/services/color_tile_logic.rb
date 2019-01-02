1757bac7965e9e06e7904072665f355dc1fc62d9
jyll sarta <illstarred.dive@gmail.com>
2019-01-03 02:21:02 +0900

    ランダム設置に一定回数以上失敗したら諦めて全探索でパネルを置きに行く

--- app/services/color_tile_logic.rb
+++ app/services/color_tile_logic.rb
@@ -4,6 +4,7 @@ module ColorTileLogic
         class NoAvailablePoints < RuntimeError ;end
         class InsufficientPoints < RuntimeError ;end
         class PositionAlreadySet < RuntimeError ;end
+        PICK_RETRY_MAX = 5
 
         attr_accessor :row, :column, :board
 
@@ -41,6 +42,11 @@ module ColorTileLogic
             exists?(x, y) && panel(x, y).block?
         end
 
+        # 座標が存在して、ブロックがない
+        def free?(x, y)
+            exists?(x, y) && !panel(x, y).block?
+        end
+
         def panel(x, y)
             return nil if !exists?(x, y)
             @board[y][x]
@@ -65,15 +71,18 @@ module ColorTileLogic
             @board[10][4].color_id = 2
             @board[12][4].color_id = 2
 
-            100.times{put_block}
+            @pairs.times{put_block}
         end
 
         # 試しに座標一つ指定してみて、そこ基準でブロックのペアを置けないならもう一度繰り返す
-        def pick_available_panel_by_random
+        def pick_puttable_panel
+            retry_count = 0
             begin
+                retry_count += 1
+                return pick_available_panel_by_scan_all if retry_count > PICK_RETRY_MAX
                 x = @seed.rand_int(@row)
                 y = @seed.rand_int(@column)
-                raise PositionAlreadySet if block?(x, y)
+                raise PositionAlreadySet unless free?(x, y)
                 raise InsufficientPoints unless Cross.new(self, panel(x,y)).puttable?
             rescue PositionAlreadySet
                 puts "board #{x}, #{y} already set... retry"
@@ -87,20 +96,22 @@ module ColorTileLogic
 
         # ブロックのペアを置ける場所をリストアップして、そこから一つ選ぶ
         def pick_available_panel_by_scan_all
-            panels.select{|panel| Cross.new(self, panel).puttable?}.sample
-        end 
+            puts "achieved retry max. start scan"
+            all_puttable_points = panels.select{|panel| Cross.new(self, panel).puttable?}
+            raise NoAvailablePoints if all_puttable_points.empty?
+
+            @seed.sample(all_puttable_points)
+        end         
 
         def put_block
-            panel = pick_available_panel_by_random
+            panel = pick_puttable_panel
 
             points = Cross.new(self, panel).available_panels
 
-            p1 = points.sample
-            p2 = (panels - [p1]).sample
+            p1 = @seed.sample(points)
+            p2 = @seed.sample(points - [p1])
+            random_color = @seed.rand_int(@colors)
 
-            random_color = (1..@colors).to_a.sample
-
-            pp [p1,p2]
             panel(p1.x, p1.y).color_id = random_color
             panel(p2.x, p2.y).color_id = random_color
         end
@@ -124,6 +135,8 @@ module ColorTileLogic
     end
 
     class Cross
+        SEARCH_DISTANCE = 5
+
         def initialize(board, panel)
             @board = board
             @panel = panel
@@ -133,13 +146,13 @@ module ColorTileLogic
         def puttable?
             x = @panel.x
             y = @panel.y
-            # 上下左右を見て、3つ以上埋まってたら置けない
+            # 上下左右を見て、空いている方向が2つ以上あればそこには設置可能
             [
-                @board.block?(x-1, y),
-                @board.block?(x, y-1),
-                @board.block?(x+1, y),
-                @board.block?(x, y+1),
-            ].select{|x| x}.length < 3
+                @board.free?(x-1, y),
+                @board.free?(x, y-1),
+                @board.free?(x+1, y),
+                @board.free?(x, y+1),
+            ].select{|b| b}.length >= 2
         end
 
         # ブロック設置候補になるパネルのリストをかえす
@@ -147,22 +160,22 @@ module ColorTileLogic
             points = []
             x = @panel.x
             y = @panel.y
-            # 5マスまで奥にブロックを探しに行くが、画面端かブロックにぶつかったらそこでその方向の探索は終了
-            distance = 2
+            # SEARCH_DISTANCEマス先まで、前方に探索していって壁かブロックにぶつかったら終了
+            distance = SEARCH_DISTANCE
             ((x-1-distance)..(x-1)).to_a.reverse.each do |ix|
-                break if !@board.exists?(ix, y) || @board.block?(ix, y)
+                break unless @board.free?(ix, y)
                 points.append(@board.panel(ix, y))
             end
             ((y-1-distance)..(y-1)).to_a.reverse.each do |iy|
-                break if !@board.exists?(x, iy) || @board.block?(x, iy)
+                break unless @board.free?(x, iy)
                 points.append(@board.panel(x, iy))
             end
             ((x+1)..(x+1+distance)).each do |ix|
-                break if !@board.exists?(ix, y) || @board.block?(ix, y)
+                break unless @board.free?(ix, y)
                 points.append(@board.panel(ix, y))
             end
             ((y+1)..(y+1+distance)).each do |iy|
-                break if !@board.exists?(x, iy) || @board.block?(x, iy)
+                break unless @board.free?(x, iy)
                 points.append(@board.panel(x, iy))
             end
             return points            
@@ -193,6 +206,11 @@ module ColorTileLogic
         def rand_int(max)
             rand = self.next
             return rand / (0xFFFFFFFF/max)
+        end
+
+        # select random one from array
+        def sample(array)
+            array[rand_int(array.length)]
         end
     end
 end
