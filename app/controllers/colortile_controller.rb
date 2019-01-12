79b291b39c5383d817f7185a381ce52922418012
jyllsarta <illstarred.dive@gmail.com>
2019-01-12 22:09:54 +0900

    盤面サイズを調整

--- app/controllers/colortile_controller.rb
+++ app/controllers/colortile_controller.rb
@@ -3,17 +3,17 @@ class ColortileController < ApplicationController
     # render index page
     def index
         # slim側と連携する必要あり
-        @row = 16
-        @column = 8
+        @row = 20
+        @column = 10
         render :index, layout: false
     end
 
     # returns new colortile board
     def new
-        x = 16
-        y = 8
+        x = 20
+        y = 10
         colors = 12
-        pairs = 55
+        pairs = 85
         seed = rand(1000)
         board = ColorTileLogic::Board.new(x, y, colors, pairs, seed)
         render json: board.to_json
