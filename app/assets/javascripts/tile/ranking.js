0e1e36f18f41e9cc256886899348196e26b05e91
jyllsarta <illstarred.dive@gmail.com>
2019-02-10 20:31:43 +0900

    ランキングで自分を強調表示

---
+++ app/assets/javascripts/tile/ranking.js
@@ -0,0 +1,37 @@
+//
+// view.js modelとviewを頑張ってつなぐ係
+// (このファイルがこんなに苦しいならフロントのライブラリを使うべきでは...?)
+//
+
+
+
+function to_trip(tr) {
+    var shaObj = new jsSHA("SHA-256", "TEXT");
+    shaObj.update(tr);
+    return shaObj.getHash("B64").slice(0, 10);
+}
+
+function trip(rawUsername) {
+    const splitted = rawUsername.replace(/📛/g, "").split("#");
+    const displayName = splitted[0];
+    if (splitted.length == 1) {
+        return displayName;
+    }
+    const tripped = this.to_trip(splitted.slice(1).join(""));
+    return `${displayName}📛${tripped}`;
+}
+
+$(function () {
+    const username = $.cookie("username");
+    const tripped = trip(username);
+    for (var r of $(".rank")) {
+        if ($(r).find(".name").text() == tripped) {
+            console.log("name!");
+            $(r).css("background-color", "#ffe8cc").css("border-radius", "20px");
+        }
+        else {
+            console.log($(r).find(".name").text());
+            console.log(tripped);
+        }
+    }
+});
