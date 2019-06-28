0372a8a5f704e5ff9889fd7df7ba1a4e552b3636
jyll sarta <illstarred.dive@gmail.com>
2019-06-29 00:00:45 +0900

    キー入力でnotesを更新する

--- app/assets/javascripts/zxcv.js
+++ app/assets/javascripts/zxcv.js
@@ -1,13 +1,61 @@
+// global constants
+const G_NOTES = {
+  0: "z",
+  1: "x",
+  2: "c",
+  3: "v",
+}
+
+
 var app = new Vue({
     el: '#app',
     data: {
-        
+      notes: [],
+      keyboard: [],
     },
     created: function(){
         console.log("pong!");
+        this.initNotes();
+        this.initKeyboard();
     },
     computed: {
     },
     methods: {
+      initNotes: function () {
+        for (let i = 0; i < 100; ++i){
+          this.notes.push(parseInt(Math.random() * 4));
+        }
+      },
+      initKeyboard: function () {
+        for(let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
+          this.keyboard[String.fromCharCode(i)] = 0;
+        }
+      },
+
+      handleKeydown: function (e) {
+        this.keyboard[e.key] = 1;
+        this.updateNotes();
+      },
+      handleKeyup: function (e) {
+        this.keyboard[e.key] = 0;
+      },
+
+      updateNotes: function(){
+        if(this.keyboard[G_NOTES[this.notes[0]]]){
+          console.log(this.notes[0]);
+          this.notes.shift();
+        }
+      }
     }
 });
+
+// init global key event
+$(function(){
+  document.onkeydown = function(e){
+    app.handleKeydown(e);
+  };
+  document.onkeyup = function(e){
+    app.handleKeyup(e);
+  };
+});
+
