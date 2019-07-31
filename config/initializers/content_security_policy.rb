36bf995debeed0118960c305860a1c2a51187cb4
jyll sarta <illstarred.dive@gmail.com>
2019-08-01 01:33:05 +0900

    productionで動かなかったのでCSP, loaderの設定変更

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,6 +2,7 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
   else
-    policy.script_src :self, :https
+    # jyllsarta.net は http なので、これがonだとvueが読めなくなる
+    #policy.script_src :self, :https
   end
 end
