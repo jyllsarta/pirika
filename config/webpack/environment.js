32a916f83b8744af0004f51272656d1d9587e060
jyll sarta <illstarred.dive@gmail.com>
2019-09-09 23:59:33 +0900

    typescriptを導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const pug = require('./loaders/pug')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('typescript', typescript)
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('pug', pug)
 module.exports = environment
