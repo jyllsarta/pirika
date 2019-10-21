8140185aae4cff514af5b31eec6d334b2e796bd3
jyll sarta <illstarred.dive@gmail.com>
2019-10-21 20:12:56 +0900

    ゲーム一覧ページに最近の作品を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
-    def full_title(content_title)
-        if !content_title
-            return "さーたはうす"
-        end
-        "#{content_title} | さーたはうす"
+  def full_title(content_title)
+    if !content_title
+      return "さーたはうす"
     end
+    "#{content_title} | さーたはうす"
+  end
+
+  def screenshot_entries(directory_name)
+    Rails.root.join("public", "images", "screenshot", directory_name).entries.reject(&:directory?).map do |filename|
+      "/images/screenshot/#{directory_name}/#{filename}"
+    end
+  end
 end
