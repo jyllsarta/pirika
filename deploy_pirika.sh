b467ef6d7c415baf6faf96eba05d004b9ce07053
Jyll Sarta <illstarred.dive@gmail.com>
2019-12-29 17:36:31 +0900

    add deploy command

---
+++ deploy_pirika.sh
@@ -0,0 +1,20 @@
+echo "cd pirika"
+cd ~/pirika
+echo "git pull"
+git pull
+echo "bundle install"
+bundle install
+echo "migrate"
+RAILS_ENV=production rails db:migrate
+echo "precompiling asset"
+RAILS_ENV=production rails assets:precompile
+
+echo "kill older server(https)"
+PID=`ps ax | grep '[p]uma' | awk '{ print $1 }'`
+echo "older server is ${PID}"
+kill ${PID}
+
+echo "start server"
+# for http start
+# RAILS_ENV=production bundle exec rails s -d
+SSL_ENABLED=y RAILS_ENV=production bundle exec pumactl start
